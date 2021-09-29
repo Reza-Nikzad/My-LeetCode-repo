18f4108cbe39f0de77198f298c4812d5840feb9e
Mohammadreza Nikzad <mohammadreza.nikzad@mail.concordia.ca>
2021-09-28 21:22:37 -0400

    Efficient prime Number

---
+++ Popular_algorithm/primeNumber.py
@@ -0,0 +1,14 @@
+def isprimeNumber(num : int): 
+    if num <= 2: 
+        return True 
+    else: 
+        i = 2
+        end = num
+        while i < end: 
+            if num % i == 0: 
+                return False
+            end = (num // i) + 1
+            i+=1 
+    return True
+
+print(isprimeNumber(12))
