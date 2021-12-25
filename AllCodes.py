39ccb947c6715c59878035aaadbb3e05a899d2cb
Mohammadreza Nikzad <mohammadreza.nikzad@mail.concordia.ca>
2021-12-25 15:20:38 -0500

    19

--- AllCodes.py
+++ AllCodes.py
@@ -841,3 +841,32 @@ while result :
     print(result.val, end=',')
     result = result.next
 #===================================================================
+# 19. Remove Nth Node from end of Linked list
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        fast = slow = head
+
+        for _ in range(n):
+            fast = fast.next
+
+        if not fast:
+            return head.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+        return head
+
+l = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+result = Solution().removeNthFromEnd(l, 4)
+while result:
+    print(result.val, end=',')
+    result = result.next
+#=============================================================================
