456ea371dbea668165172a6691544f7938dcf010
Mohammadreza Nikzad <mohammadreza.nikzad@mail.concordia.ca>
2022-01-04 13:36:09 -0500

    33

--- AllCodes.py
+++ AllCodes.py
@@ -1413,3 +1413,32 @@ while head:
     print(head.val, end='->')
     head = head.next
 #==============================================================================
+# 33. Search in Rotated Sorted Array
+
+class RotatedSortedSearch():
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums)-1
+
+        while r >= l :
+            mid = (r+l)//2
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] < nums[r]:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else:
+                if nums[l]<= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+nums = [4,5,6,7,0,1,2] # [4,5,6,7,0,1,2] # [1]
+target = 0 # 3 -> -1 # 0 -> -1
+print(RotatedSortedSearch().search(nums,target))
+#=================================================================
+
+
